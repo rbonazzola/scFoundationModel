a34dd81f44485641b2b25f4eb5bd7acd1b838853
rbonazzola <rodbonazzola@gmail.com>
2025-03-10 13:21:32 -0300

    Initial commit which makes this repository into a package.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # scFoundationModel
-A foundation model for single-cell RNA-Seq data of Arabidopsis thaliana.
+A foundation model for single-cell RNA-Seq data of _Arabidopsis thaliana_.
+
+Install this package with `pip install .` or `pip install -e .` (editable mode).
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scFoundationModel",
+    version="0.0.1",
+    author="Rodrigo Bonazzola (rbonazzola)",
+    author_email="rodbonazzola@gmail.com",
+    description="A brief description of your project",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/rbonazzola/scFoundationModel",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "scanpy",
+        "torch",
+        "torchvision",
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "anndata",
+        "transformers",
+        "pytorch-lightning",
+        "mlflow"
+    ],
+)
