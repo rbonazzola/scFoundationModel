c622e0d5ab41e1b36841bf18b00820b2310c7bd0
rbonazzola <rodbonazzola@gmail.com>
2025-04-07 22:12:39 -0300

    Added training script

---
+++ train.sh
@@ -0,0 +1,8 @@
+python train.py \
+    --data_path data/transforms/root \
+    --compile \
+    --depth 1 \
+    --batch_size 16 \
+    --lr 1e-4 \
+    --num_workers 8 \
+    --mask_probability 0.20
