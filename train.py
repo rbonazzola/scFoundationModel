34106e1dca00ca7e612046ecf5f56a4cfb5ababd
rbonazzola <rodbonazzola@gmail.com>
2025-04-24 15:33:33 -0300

    Removed utils.py and moved save_ckpt to train.py

--- train.py
+++ train.py
@@ -19,16 +19,32 @@ import dagshub
 from tqdm import tqdm
 
 SEED = 42
-
 torch.set_float32_matmul_precision('high')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 dagshub.init("scFoundationModel", "rbonazzola", mlflow=True)
 
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters())
+
+
+def save_ckpt(epoch, model, optimizer, scheduler, losses, model_name, ckpt_folder):
+    """
+    save checkpoint
+    """
+    if not os.path.exists(ckpt_folder):
+        os.makedirs(ckpt_folder)
+    torch.save(
+        {
+            'epoch': epoch,            
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+            'losses': losses,
+        },
+        f'{ckpt_folder}/{model_name}_{epoch}.pth'
+    )
 
 
 class Trainer:
